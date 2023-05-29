832258f879073381bdf2b2612b1ae53cc119d33a
Peyton Kromash <pkromash@gmail.com>
2023-05-29 12:20:38 -0400

    m

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ app.use(express.urlencoded({limit: "30mb", extended: true}));
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://p-chat-kohl.vercel.app");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PATCH, DELETE"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
